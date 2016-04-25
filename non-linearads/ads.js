87ae600160aa8e1df492bb84e9db27a3d19ac233
binny01 <binnyp@google.com>
2016-04-25 14:07:17 -0400

    change

--- non-linearads/ads.js
+++ non-linearads/ads.js
@@ -69,7 +69,7 @@ Ads.prototype.resume = function() {
 
 Ads.prototype.resize = function(width, height) {
   if (this.adsManager_) {
-    this.adsManager_.resize(width, height, google.ima.ViewMode.FULLSCREEN);
+    this.adsManager_.resize(400, 50, google.ima.ViewMode.FULLSCREEN);
   }
 };
 
