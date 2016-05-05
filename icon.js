9b5f0d8d455c2bccbfc32683d434235b8822c4ce
binny01 <binnyp@google.com>
2016-05-05 15:41:49 -0400

    new file

---
+++ icon.js
@@ -0,0 +1,12 @@
+<script>
+
+ (function() {
+ 	var vediodiv =document.getElementByID('adcontainer')
+  
+    var img = document.createElement("IMG");
+    x.setAttribute("src", "http://placehold.it/96x15");
+    x.setAttribute("width", "304");
+    x.setAttribute("width", "228");
+    document.body.appendChild(adcontainer);
+})
+</script>
