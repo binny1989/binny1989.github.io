0ec66ce8a1e10085b6018301da20687cc97a4662
binny01 <binnyp@google.com>
2016-05-05 16:38:03 -0400

    commit

--- icon.js
+++ icon.js
@@ -1,12 +1,9 @@
-<script>
-
- (function() {
- 	var vediodiv =document.getElementByID('adcontainer')
-  
-    var img = document.createElement("IMG");
-    x.setAttribute("src", "http://placehold.it/96x15");
-    x.setAttribute("width", "304");
-    x.setAttribute("width", "228");
-    document.body.appendChild(adcontainer);
-})
-</script>
+function show_image(src, width, height, alt) {
+        var img = document.createElement("img");
+        img.src = "https://placeholdit.imgix.net/~text?txtsize=5&txt=96%C3%9715&w=96&h=15&txtpad=1";
+        img.width = width;
+        img.height = height;
+        img.alt = alt;
+        document.body.appendChild(img);
+    }
+            show_image("https://placeholdit.imgix.net/~text?txtsize=5&txt=96%C3%9715&w=96&h=15&txtpad=1", 100,50, "Google Logo");
