b27f4e64cadca171ac59d34e1e82c9c036cba9d5
binny01 <binnyp@google.com>
2016-05-06 14:25:16 -0400

    z-index

--- icon.js
+++ icon.js
@@ -3,5 +3,6 @@ var x = document.createElement("IMG");
     x.setAttribute("src", "http://placehold.it/96x15");
     x.setAttribute("width", "60");
     x.setAttribute("height", "20");
+    x.setAttribute("z-index","-1");
     document.body.appendChild(x);
 }
