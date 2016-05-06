8c33ee7671775e88cd22753529644c300e135799
binny01 <binnyp@google.com>
2016-05-06 12:42:55 -0400

    change in height and width

--- icon.js
+++ icon.js
@@ -1,7 +1,7 @@
 window.onload = function(){
 var x = document.createElement("IMG");
     x.setAttribute("src", "http://placehold.it/96x15");
-    x.setAttribute("width", "304");
-    x.setAttribute("height", "228");
+    x.setAttribute("width", "60");
+    x.setAttribute("height", "20");
     document.body.appendChild(x);
 }
