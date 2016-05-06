aab2bb60bc501af559eef44e7dbe9b65a843feef
binny01 <binnyp@google.com>
2016-05-06 14:31:30 -0400

    new change

--- icon.js
+++ icon.js
@@ -3,6 +3,6 @@ var x = document.createElement("IMG");
     x.setAttribute("src", "http://placehold.it/96x15");
     x.setAttribute("width", "60");
     x.setAttribute("height", "20");
-    x.setAttribute("z-index","-1");
     document.body.appendChild(x);
+   document.getElementsByTagName('x') .style.zIndex='3';
 }
