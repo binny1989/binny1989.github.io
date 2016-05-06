caceffd4c4d09c7c7769df058e81e500f18bd9af
binny01 <binnyp@google.com>
2016-05-06 15:59:42 -0400

    div

--- icon.js
+++ icon.js
@@ -1,9 +1,13 @@
 window.onload = function(){
+var mydiv= document.createElement("div");
+	mydiv.setAttribute("id","myid");
+	mydiv.setAttribute("width","70");
+	mydiv.setAttribute("height","30");
 var myIMG = document.createElement("IMG");
     myIMG.setAttribute("src", "http://placehold.it/96x15");
     myIMG.setAttribute("width", "60");
     myIMG.setAttribute("height", "20");
-    document.body.appendChild(myIMG);
+    document.getElementsByTagName('div').appendChild(myIMG);
     document.getElementsByTagName('IMG').style.zIndex='1000';
 
 }
