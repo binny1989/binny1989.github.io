5b40565450d61ad7f72cecf14b70f3c2711c24ce
binny01 <binnyp@google.com>
2016-05-05 21:08:38 -0400

    commit

--- icon.js
+++ icon.js
@@ -1,5 +1,5 @@
 function show_image(src, width, height, alt) {
-        var div = document.getElementByID('adcontainer')
+        var div = document.getElementByID('adcontainer');
         var img = document.createElement("img");
         img.src = "https://placeholdit.imgix.net/~text?txtsize=5&txt=96%C3%9715&w=96&h=15&txtpad=1";
         img.width = width;
