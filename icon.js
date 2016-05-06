c6772c974f200f5f29fd8f2002a5264632e7eb50
binny01 <binnyp@google.com>
2016-05-06 16:21:12 -0400

    new

--- icon.js
+++ icon.js
@@ -9,7 +9,7 @@ var myIMG = document.createElement("IMG");
     myIMG.setAttribute("src", "http://placehold.it/96x15");
     myIMG.setAttribute("width", "60");
     myIMG.setAttribute("height", "20");
-    document.body.appendChild(myIMG);
+    mydiv.appendChild(myIMG);
     document.getElementsByTagName('IMG').style.zIndex='1000';
 
 }
