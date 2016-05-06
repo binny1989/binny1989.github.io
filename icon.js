0565a22e0b6fecc6f43854e454bac90aa86539ef
binny01 <binnyp@google.com>
2016-05-05 21:08:04 -0400

    new

--- icon.js
+++ icon.js
@@ -1,9 +1,10 @@
 function show_image(src, width, height, alt) {
+        var div = document.getElementByID('adcontainer')
         var img = document.createElement("img");
         img.src = "https://placeholdit.imgix.net/~text?txtsize=5&txt=96%C3%9715&w=96&h=15&txtpad=1";
         img.width = width;
         img.height = height;
         img.alt = alt;
-        document.body.appendChild(img);
+        document.div.appendChild(img);
     }
-            show_image("https://placeholdit.imgix.net/~text?txtsize=5&txt=96%C3%9715&w=96&h=15&txtpad=1", 100,50, "Google Logo");
+     show_image("https://placeholdit.imgix.net/~text?txtsize=5&txt=96%C3%9715&w=96&h=15&txtpad=1", 100,50, "Google Logo");
