30420f4f3541adaa0b74447a56b783a0dcbbe5c1
binny01 <binnyp@google.com>
2016-05-05 21:53:00 -0400

    new

--- icon.js
+++ icon.js
@@ -1,5 +1,7 @@
+window.onload = function(){
 var x = document.createElement("IMG");
     x.setAttribute("src", "http://placehold.it/96x15");
     x.setAttribute("width", "304");
     x.setAttribute("width", "228");
     document.body.appendChild(x);
+}
