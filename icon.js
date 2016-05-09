5addbb921cd323f4ffee6809018b79ae2532110a
binny01 <binnyp@google.com>
2016-05-09 10:31:17 -0400

    new

--- icon.js
+++ icon.js
@@ -1,11 +1,13 @@
+
 window.onload = function(){
 var mydiv= document.createElement("div");
 	mydiv.setAttribute("id","myid");
 	mydiv.setAttribute("width","70");
 	mydiv.setAttribute("height","30");
 	mydiv.setAttribute("onClick","window.location='https://www.facebook.com'");
+	document.getElementsByTagName('div').style.zIndex='1000';
 	document.body.appendChild(mydiv);
-	console.log(document.getElementsByTagName("div"));
+
 var myIMG = document.createElement("IMG");
     myIMG.setAttribute("src", "http://placehold.it/96x15");
     myIMG.setAttribute("width", "60");
@@ -13,5 +15,4 @@ var myIMG = document.createElement("IMG");
     mydiv.appendChild(myIMG);
     document.getElementsByTagName('IMG').style.zIndex='1000';
     
-
 }
