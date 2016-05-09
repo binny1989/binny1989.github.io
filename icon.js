0d61f4ec44e8183c984d4b331dd48556e3ea7ad0
binny01 <binnyp@google.com>
2016-05-09 10:34:19 -0400

    update

--- icon.js
+++ icon.js
@@ -4,14 +4,12 @@ var mydiv= document.createElement("div");
 	mydiv.setAttribute("id","myid");
 	mydiv.setAttribute("width","70");
 	mydiv.setAttribute("height","30");
-	mydiv.setAttribute("onClick","window.location='https://www.facebook.com'");
 	document.body.appendChild(mydiv);
-	document.getElementsByTagName('div').style.zIndex='1000';
-
 var myIMG = document.createElement("IMG");
     myIMG.setAttribute("src", "http://placehold.it/96x15");
     myIMG.setAttribute("width", "60");
     myIMG.setAttribute("height", "20");
+    myIMG.setAttribute("onClick","window.location='https://www.facebook.com'");
     mydiv.appendChild(myIMG);
     document.getElementsByTagName('IMG').style.zIndex='1000';
     
