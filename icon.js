4db5324d9943c39d13af813052011359a4b0e0d5
binny01 <binnyp@google.com>
2016-05-09 10:25:54 -0400

    commit

--- icon.js
+++ icon.js
@@ -2,7 +2,8 @@ window.onload = function(){
 var mydiv= document.createElement("div");
 	mydiv.setAttribute("id","myid");
 	mydiv.setAttribute("width","70");
-	mydiv.setAttribute("height","30")
+	mydiv.setAttribute("height","30");
+	mydiv.setAttribute("onClick","window.location='https://www.facebook.com'");
 	document.body.appendChild(mydiv);
 	console.log(document.getElementsByTagName("div"));
 var myIMG = document.createElement("IMG");
@@ -11,5 +12,6 @@ var myIMG = document.createElement("IMG");
     myIMG.setAttribute("height", "20");
     mydiv.appendChild(myIMG);
     document.getElementsByTagName('IMG').style.zIndex='1000';
+    
 
 }
