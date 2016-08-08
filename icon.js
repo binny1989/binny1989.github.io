9679bed0e28f1170acdb51b6f5591d7af1952938
binny01 <binnyp@google.com>
2016-08-08 11:45:28 -0400

    nonlinear

--- icon.js
+++ icon.js
@@ -2,13 +2,14 @@
 window.onload = function(){
 var mydiv= document.createElement("div");
 	mydiv.setAttribute("id","myid");
-	mydiv.setAttribute("width","70");
-	mydiv.setAttribute("height","30");
+	mydiv.setAttribute("width","310");
+	mydiv.setAttribute("height","260");
 	document.body.appendChild(mydiv);
 var myIMG = document.createElement("IMG");
-    myIMG.setAttribute("src", "http://placehold.it/96x15");
-    myIMG.setAttribute("width", "60");
-    myIMG.setAttribute("height", "20");
+    myIMG.setAttribute("src", "http://s0.2mdn.net/1248596/PID_1472073_1290217213786_PID_1244559_1265644904000_parisian_love_300x250.jpg
+");
+    myIMG.setAttribute("width", "300");
+    myIMG.setAttribute("height", "250");
     myIMG.setAttribute("onClick","window.location='https://www.facebook.com'");
     mydiv.appendChild(myIMG);
     document.getElementsByTagName('IMG').style.zIndex='1000';
