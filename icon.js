78419fa3b365a9915d3d967b84f7ada21e440479
binny01 <binnyp@google.com>
2016-08-08 11:48:36 -0400

    update

--- icon.js
+++ icon.js
@@ -2,13 +2,13 @@
 window.onload = function(){
 var mydiv= document.createElement("div");
 	mydiv.setAttribute("id","myid");
-	mydiv.setAttribute("width","310");
+	mydiv.setAttribute("width","410");
 	mydiv.setAttribute("height","260");
 	document.body.appendChild(mydiv);
 var myIMG = document.createElement("IMG");
-    myIMG.setAttribute("src", "http://s0.2mdn.net/1248596/PID_1472073_1290217213786_PID_1244559_1265644904000_parisian_love_300x250.jpg
+    myIMG.setAttribute("src", "http://s0.2mdn.net/1379578/PID_1244559_1281625520000_google.jpg
 ");
-    myIMG.setAttribute("width", "300");
+    myIMG.setAttribute("width", "400");
     myIMG.setAttribute("height", "250");
     myIMG.setAttribute("onClick","window.location='https://www.facebook.com'");
     mydiv.appendChild(myIMG);
